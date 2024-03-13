8bcead578300c54c647be9623bd79e7ff3060cb5
root <lucilio@codeby.com.br>
2024-03-13 01:26:53 -0300

    fix: dump path

--- main.go
+++ main.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"log"
+	"os"
+	"path"
+	"strings"
 
 	"github.com/LucilioCampos/mynes/ui"
 )
 
 func main() {
 	log.SetFlags(0)
-	ui.Run("/root/workspace/nes/Super Mario Bros. (Japan, USA).nes")
+	ui.Run(getPath()[0])
+}
+
+func getPath() []string {
+	var arg string
+	args := os.Args[1:]
+	if len(args) == 1 {
+		arg = args[0]
+	} else {
+		arg, _ = os.Getwd()
+	}
+	info, err := os.Stat(arg)
+	if err != nil {
+		return nil
+	}
+	if info.IsDir() {
+		infos, err := os.ReadDir(arg)
+		if err != nil {
+			return nil
+		}
+		var result []string
+		for _, info := range infos {
+			name := info.Name()
+			if !strings.HasSuffix(name, ".nes") {
+				continue
+			}
+			result = append(result, path.Join(arg, name))
+		}
+		return result
+	} else {
+		return []string{arg}
+	}
 }
